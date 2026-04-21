90c6087ad291611f1ca2622504d9fbdc81e5a4d9
kavinduMarasinghe <kavinduwethmin@gmail.com>
2026-04-21 16:58:48 +0530

    Add update event request form layout

--- Frontend/src/Components/events/EventRequestForm.jsx
+++ Frontend/src/Components/events/EventRequestForm.jsx
@@ -1672,7 +1672,15 @@ function EventRequestForm() {
                     error={errors.clearTime}
                     icon={FiClock}
                   />
-                  
+                  <InputField
+                    label="Venue"
+                    name="venue"
+                    value={formData.venue}
+                    onChange={handleChange}
+                    error={errors.venue}
+                    icon={FiMapPin}
+                    placeholder="Main Auditorium"
+                  />
                   <SelectField
                     label="Venue Type"
                     name="venueType"
