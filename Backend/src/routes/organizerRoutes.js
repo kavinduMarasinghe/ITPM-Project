121cdbedd32685122d7ae3a1bf6c2e7db491c298
kavinduMarasinghe <kavinduwethmin@gmail.com>
2026-04-22 16:06:14 +0530

    Add update organizer routes

--- Backend/src/routes/organizerRoutes.js
+++ Backend/src/routes/organizerRoutes.js
@@ -19,5 +19,16 @@ module.exports = [
     roles: [ROLES.ADMIN, ROLES.HOD],
     handler: organizerController.getOrganizers,
   },
-  
+  {
+    method: "GET",
+    path: "/api/organizers/:id",
+    roles: [ROLES.ADMIN, ROLES.HOD],
+    handler: organizerController.getOrganizerById,
+  },
+  {
+    method: "PUT",
+    path: "/api/organizers/:id/status",
+    roles: [ROLES.ADMIN, ROLES.HOD],
+    handler: organizerController.updateOrganizerStatus,
+  },
 ];
